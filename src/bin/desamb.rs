7e84c4045ea69ce8c0b4fbd94f17b9326a2dd75b
Ondřej Herman <ondrej.herman@sketchengine.co.uk>
2023-02-13 20:26:13 +0100

    desamb: update output format

--- src/bin/desamb.rs
+++ src/bin/desamb.rs
@@ -4,13 +4,38 @@ use ndarray::prelude::*;
 use adagram::adagram::VectorModel;
 use adagram::common::*;
 
+use clap::Parser;
+
+const VERSION: &str = git_version::git_version!(args=["--tags","--always", "--dirty"]);
+
+/// Assign concordance lines to senses
+#[derive(Parser, Debug)]
+#[clap(author, version=VERSION, about)]
+struct Args {
+    /// adagram model path
+    model: String,
+
+    /// ignore the headword during desambiguation
+    #[clap(long,default_value_t=true)]
+    skip_headword: bool,
+
+    /// append the result to the input instead of outputting the classes only
+    #[clap(long,default_value_t=true)]
+    mirror_input: bool,
+
+    // output probabilities for all senses
+    #[clap(long,default_value_t=true)]
+    print_probs: bool,
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
     if std::env::args().count() != 2 {
         return Err("usage: desamb MODELPATH".into());
     }
 
     // load model
-    let modelpath = std::env::args().nth(1).ok_or("model path missing")?;
+    let modelpath = args.model;
     let (vm, id2str) = VectorModel::load_model(&modelpath)?;
 
     // build reverse lexicon mapping
@@ -19,22 +44,27 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         str2id.insert(word, id as u32);
     }
 
+    let mut _headword_not_in_lexicon = 0;
+    let mut _no_context = 0;
+    let mut _all_invalid = 0;
+
     let mut z = Array::<f64, Ix1>::zeros(vm.nmeanings());
-    for line in std::io::stdin().lines() {
-        let unwrapped = line?;
-        let mut parts = unwrapped.split_whitespace();
-        let head = match parts.next() {
+    for maybeline in std::io::stdin().lines() {
+        let line = maybeline?;
+        let mut cols = line.split('\t');
+        let head = match cols.next() {
             Some(x) => x,
             None => {
-                println!("=== EMPTY INPUT ===");
+                eprintln!("=== EMPTY INPUT ===");
                 continue
             },
         };
-        
+
         let x = match str2id.get(head) {
             Some(n) => *n,
             None => {
-                println!("=== HEADWORD NOT IN LEXICON: {} ===", head);
+                eprintln!("=== HEADWORD NOT IN LEXICON: {} ===", head);
+                _headword_not_in_lexicon += 1;
                 continue;
             },
         };
@@ -48,15 +78,29 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         let mut _nvalid = 0;
         let mut _ninvalid = 0;
-        for ctxword in parts {
-            let y = match str2id.get(ctxword) {
-                Some(n) => { _nvalid += 1; *n },
-                None => { _ninvalid += 1; continue; },
-            };
-            var_update_z(&vm.in_vecs, &vm.out_vecs, &vm.code, &vm.path, x, y, &mut z);
+
+        match cols.next() {
+            None => {
+                _no_context += 1;
+            },
+            Some(context) => {
+                let parts = context.split_whitespace();
+                for ctxword in parts {
+                    if args.skip_headword && ctxword == head {
+                        continue;
+                    }
+                    let y = match str2id.get(ctxword) {
+                        Some(n) => { _nvalid += 1; *n },
+                        None => { _ninvalid += 1; continue; },
+                    };
+                    var_update_z(&vm.in_vecs, &vm.out_vecs, &vm.code, &vm.path, x, y, &mut z);
+                }
+            },
         }
 
-        // if nvalid < 1;
+        if _nvalid < 1 {
+            _all_invalid += 1;
+        }
 
         exp_normalize(&mut z);
 
@@ -65,18 +109,29 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             .max_by(|(_, a), (_, b)| a.total_cmp(b))
             .map(|(i, _)| i);
 
+        if args.mirror_input {
+            print!("{}", line);
+        }
+
         if let Some(mp) = maxpos {
             print!("\t{}", mp);
         } else {
-            print!("\t-");
+            print!("\t-1");
         }
 
-        println!("{}", z.iter()
-                 .enumerate()
-                 .map(|(i, p): (usize, &f64)|->String {format!("{}:{:.2}", i, p)})
-                 .collect::<Vec<String>>()  // won't be necessary in future rust
-                 .join(" "));
+        if args.print_probs {
+            print!("\t{}", z.iter()
+                     .enumerate()
+                     .map(|(i, p): (usize, &f64)|->String {format!("{}:{:.2}", i, p)})
+                     .collect::<Vec<String>>()  // won't be necessary in future rust
+                     .join(" "));
+        }
+        println!();
     }
+
+    eprintln!("encountered {} lines with no context", _no_context);
+    eprintln!("encountered {} headwords not present in lexicon", _headword_not_in_lexicon);
+    eprintln!("encountered {} lines where no context words were found in lexicon", _all_invalid);
 
     Ok(())
 }
