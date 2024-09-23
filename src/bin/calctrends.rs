8d822febc9c1c456c36c679697551d3d0c29fc00
Ondřej Herman <ondrej.hermana@sketchengine.co.uk>
2024-09-23 12:58:03 +0200

    reformat diachronic

--- src/bin/calctrends.rs
+++ src/bin/calctrends.rs
@@ -49,71 +49,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     } else { args.trendbase };
     eprintln!("writing output to {}", trendbase);
 
-    let diavals: Vec<_> = (0..diastructattr.id_range())
-        .map(|did| diastructattr.id2str(did))
-        .collect();
-
-    // eprintln!("diavals: {:?}", diavals);
-    let bad_values = std::collections::HashSet::from([
-        "===NONE===",
-        "===NONE==",
-        "===NONE=",
-        "===NONE",
-        "===NON",
-        "===NO",
-        "===N",
-        "9999",
-        "",
-    ]);
-
-    let to = tag_ordering(&diavals);
-    let mut diamap = [u32::MAX].into_iter().cycle().take(diastructattr.id_range() as usize)
-        .collect::<Vec<u32>>();
-    let norms = diastructattr.get_freq("token:l")?;
-    let mut epoch_count = 0;
-    let mut total_norm = 0;
-    for (orgid, s, _key) in &to {
-        if !bad_values.contains(s) {
-            epoch_count += 1;
-            total_norm += norms.frq(*orgid as u32);
-        }
-    }
-
-    let min_norm = args.epoch_limit;
-    let avg_norm = total_norm as f64 / epoch_count as f64;
-    eprintln!("average norm is {}", avg_norm);
-    let min_norm = if min_norm >= 1. { min_norm as u64 }
-    else {
-        let adj_min_norm = (avg_norm * min_norm) as u64;
-        eprintln!("adjusted EPOCH_LIMIT is {}", adj_min_norm);
-        adj_min_norm
-    };
-
-    fn format_key(key: &Vec<usize>) -> String {
-        "<".to_string() + &key.iter().map(|v| format!("{}", v))
-            .collect::<Vec<_>>().join(", ") + ">"
-    }
-
-    let mut newid = 0;
-    let mut new_norms = Vec::<f64>::new();
-    eprintln!("values ordered as (index, value, norm, original index, key):");
-    for (orgid, s, key) in &to {
-        let norm = norms.frq(*orgid as u32);
-        if bad_values.contains(s) || norm < min_norm {
-            eprintln!("\t-\t{}\t{}\t{}\t{}",
-                s, norm, *orgid, format_key(&key));
-        } else {
-            eprintln!("\t{}\t{}\t{}\t{}\t{}",
-                newid, s, norm, *orgid, format_key(&key));
-            diamap[*orgid] = newid;
-            newid += 1;
-            new_norms.push(norm as f64);
-        }
-    }
+    let (diamap, new_norms) = map_diavals(diastructattr.as_ref(), args.epoch_limit)?;
 
     let h = posattr.id_range() as usize;
-    let w = newid as usize;
-    eprintln!("dl {}", diavals.len());
+    let w = new_norms.len();
     eprintln!("w {}", w);
 
     if w < 2 {
@@ -230,64 +169,4 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     eprintln!("done.");
     Ok(())
 }
-
-struct BinaryTrendsWriter<'a> { dest: &'a mut dyn std::io::Write, }
-impl BinaryTrendsWriter<'_> {
-    fn new(dest: &'_ mut dyn std::io::Write) -> Result<BinaryTrendsWriter<'_>, Box<dyn std::error::Error>> {
-        let mut header = [0u8; 32];
-        for (i, e) in b"manatee trends v1.1".iter().enumerate() {
-            header[i] = *e;
-        }
-        dest.write_all(&header)?;
-        Ok(BinaryTrendsWriter { dest })
-    }
-    fn put(&mut self, id: u32, slope: i8, p: f32) -> Result<(), Box<dyn std::error::Error>> {
-        self.dest.write_all(&id.to_le_bytes())?;
-        self.dest.write_all(&slope.to_le_bytes())?;
-        self.dest.write_all(&p.to_le_bytes())?;
-        Ok(())
-    }
-}
-
-
-fn clamp(n: u8, max: u8) -> u8 { if n > max { max } else { n } }
-
-struct BinaryMinigraphWriter<'a> { dest: &'a mut dyn std::io::Write, }
-impl BinaryMinigraphWriter<'_> {
-    fn new(dest: &'_ mut dyn std::io::Write) -> Result<BinaryMinigraphWriter<'_>, Box<dyn std::error::Error>> {
-        let mut header = [0u8; 32];
-        for (i, e) in b"manatee minigraphs v1.1".iter().enumerate() {
-            header[i] = *e;
-        }
-        dest.write_all(&header)?;
-        Ok(BinaryMinigraphWriter { dest })
-    }
-    fn put(&mut self, id: u32, nums: &[u8]) -> Result<(), Box<dyn std::error::Error>> {
-        self.dest.write_all(&id.to_le_bytes())?;
-        let v: u32 = nums.into_iter().enumerate().map(|(i, &n)| (clamp(n, 15) as u32) << (i * 4)).sum();
-        self.dest.write_all(&v.to_le_bytes())?;
-        Ok(())
-    }
-}
-
-fn resample(from: &[f64], to: &mut [f64]) {
-    let frompart = 1.0f64 / from.len() as f64;
-    let topart = 1.0f64 / to.len() as f64;
-    if to.len() > from.len() {
-        for i in 0..to.len() {
-            let frombeg = ((frompart * (i as f64)) / topart) as usize;
-            let fromend = ((frompart * (i as f64) + 1.0) / topart) as usize;
-            for j in frombeg..fromend {
-                to[i] += from[j] / (fromend - frombeg) as f64;
-            }
-        }
-    } else {
-        for i in 0..to.len() {
-            let topos = i as f64 * topart;
-            let j = (topos / frompart) as usize;
-            to[i] = from[j];
-        }
-    }
-}
-
 
--- src/diachronic.rs
+++ src/diachronic.rs
@@ -225,3 +225,125 @@ mod tests {
     }
 }
 
+fn clamp(n: u8, max: u8) -> u8 { if n > max { max } else { n } }
+
+pub struct BinaryTrendsWriter<'a> { dest: &'a mut dyn std::io::Write, }
+impl BinaryTrendsWriter<'_> {
+    pub fn new(dest: &'_ mut dyn std::io::Write) -> Result<BinaryTrendsWriter<'_>, Box<dyn std::error::Error>> {
+        let mut header = [0u8; 32];
+        for (i, e) in b"manatee trends v1.1".iter().enumerate() {
+            header[i] = *e;
+        }
+        dest.write_all(&header)?;
+        Ok(BinaryTrendsWriter { dest })
+    }
+    pub fn put(&mut self, id: u32, slope: i8, p: f32) -> Result<(), Box<dyn std::error::Error>> {
+        self.dest.write_all(&id.to_le_bytes())?;
+        self.dest.write_all(&slope.to_le_bytes())?;
+        self.dest.write_all(&p.to_le_bytes())?;
+        Ok(())
+    }
+}
+
+pub struct BinaryMinigraphWriter<'a> { dest: &'a mut dyn std::io::Write, }
+impl BinaryMinigraphWriter<'_> {
+    pub fn new(dest: &'_ mut dyn std::io::Write) -> Result<BinaryMinigraphWriter<'_>, Box<dyn std::error::Error>> {
+        let mut header = [0u8; 32];
+        for (i, e) in b"manatee minigraphs v1.1".iter().enumerate() {
+            header[i] = *e;
+        }
+        dest.write_all(&header)?;
+        Ok(BinaryMinigraphWriter { dest })
+    }
+    pub fn put(&mut self, id: u32, nums: &[u8]) -> Result<(), Box<dyn std::error::Error>> {
+        self.dest.write_all(&id.to_le_bytes())?;
+        let v: u32 = nums.into_iter().enumerate().map(|(i, &n)| (clamp(n, 15) as u32) << (i * 4)).sum();
+        self.dest.write_all(&v.to_le_bytes())?;
+        Ok(())
+    }
+}
+
+pub fn resample(from: &[f64], to: &mut [f64]) {
+    let frompart = 1.0f64 / from.len() as f64;
+    let topart = 1.0f64 / to.len() as f64;
+    if to.len() > from.len() {
+        for i in 0..to.len() {
+            let frombeg = ((frompart * (i as f64)) / topart) as usize;
+            let fromend = ((frompart * (i as f64) + 1.0) / topart) as usize;
+            for j in frombeg..fromend {
+                to[i] += from[j] / (fromend - frombeg) as f64;
+            }
+        }
+    } else {
+        for i in 0..to.len() {
+            let topos = i as f64 * topart;
+            let j = (topos / frompart) as usize;
+            to[i] = from[j];
+        }
+    }
+}
+
+pub fn map_diavals(diastructattr: &dyn corp::corp::Attr, epoch_limit: f64)
+        -> Result<(Vec<u32>, Vec<f64>), Box<dyn std::error::Error>> {
+    let diavals: Vec<_> = (0..diastructattr.id_range())
+        .map(|did| diastructattr.id2str(did))
+        .collect();
+
+    // eprintln!("diavals: {:?}", diavals);
+    let bad_values = std::collections::HashSet::from([
+        "===NONE===",
+        "===NONE==",
+        "===NONE=",
+        "===NONE",
+        "===NON",
+        "===NO",
+        "===N",
+        "9999",
+        "",
+    ]);
+
+    let to = tag_ordering(&diavals);
+    let mut diamap = [u32::MAX].into_iter().cycle().take(diastructattr.id_range() as usize)
+        .collect::<Vec<u32>>();
+    let norms = diastructattr.get_freq("token:l")?;
+    let mut epoch_count = 0;
+    let mut total_norm = 0;
+    for (orgid, s, _key) in &to {
+        if !bad_values.contains(s) {
+            epoch_count += 1;
+            total_norm += norms.frq(*orgid as u32);
+        }
+    }
+
+    let avg_norm = total_norm as f64 / epoch_count as f64;
+    eprintln!("average norm is {}", avg_norm);
+    let min_norm = if epoch_limit >= 1. { epoch_limit as u64 }
+    else {
+        let adj_min_norm = (avg_norm * epoch_limit) as u64;
+        eprintln!("adjusted EPOCH_LIMIT is {}", adj_min_norm);
+        adj_min_norm
+    };
+
+    fn format_key(key: &Vec<usize>) -> String {
+        "<".to_string() + &key.iter().map(|v| format!("{}", v))
+            .collect::<Vec<_>>().join(", ") + ">"
+    }
+
+    let mut newid = 0;
+    let mut new_norms = Vec::<f64>::new();
+    eprintln!("values ordered as (index, value, norm, original index, key):");
+    for (orgid, s, key) in &to {
+        let norm = norms.frq(*orgid as u32);
+        if bad_values.contains(s) || norm < min_norm {
+            eprintln!("\t-\t{}\t{}\t{}\t{}",
+                s, norm, *orgid, format_key(&key));
+        } else {
+            eprintln!("\t{}\t{}\t{}\t{}\t{}",
+                newid, s, norm, *orgid, format_key(&key));
+            diamap[*orgid] = newid;
+            newid += 1;
+            new_norms.push(norm as f64);
+        }
+    }
+    Ok((diamap, new_norms))
+}
