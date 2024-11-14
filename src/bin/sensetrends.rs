ebeed9b7de5a435be5e69cbda0d2accd49a26021
Ondrej Herman <xherman1@fi.muni.cz>
2024-11-14 15:20:01 +0100

    more sensetrends

--- src/bin/sensetrends.rs
+++ src/bin/sensetrends.rs
@@ -98,8 +98,46 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut lctx = Vec::<u32>::with_capacity(ntokens);
     let mut rctx = Vec::<u32>::with_capacity(ntokens);
 
+
+    // diachronic init
+    let h = posattr.id_range() as usize;
+    let epochcnt = new_norms.len();
+    eprintln!("w {}", epochcnt);
+
+    if epochcnt < 2 {
+        eprintln!("WARNING: fewer than 2 valid structattr values");
+        return Err("semantic error".into());
+    }
+
+    if h <= 0 {
+        eprintln!("WARNING: empty corpus");
+        return Err("semantic error".into());
+    }
+
+    let mut normed = vec![0.0f64; epochcnt];
+    let mut rel = vec![0.0f64; epochcnt];
+    //let mut next_report_time = Instant::now() + Duration::from_secs(60);
+    let xs = (0..epochcnt).map(|x| x as f64).collect::<Vec<_>>();
+
+    // let mut next_report_time = Instant::now() + Duration::from_secs(60);
+    /*
+    let mut mktf = std::fs::File::create(trendbase.clone() + ".mkts_all.trends")?;
+    let mut mktwr = BinaryTrendsWriter::new(&mut mktf)?;
+    let mut mkmf = std::fs::File::create(trendbase.clone() + ".mkts_all.minigraphs")?;
+    let mut mkmwr = BinaryMinigraphWriter::new(&mut mkmf)?;
+
+    let mut lrtf = std::fs::File::create(trendbase.clone() + ".linreg_all.trends")?;
+    let mut lrtwr = BinaryTrendsWriter::new(&mut lrtf)?;
+    let mut lrmf = std::fs::File::create(trendbase.clone() + ".linreg_all.minigraphs")?;
+    let mut lrmwr = BinaryMinigraphWriter::new(&mut lrmf)?;
+    */
+
+    let nmeanings = vm.nmeanings() as usize;
+    let mut sense_diacnts = vec![0u64; nmeanings * epochcnt];
+
     eprintln!("ready");
     for line in std::io::stdin().lines() {
+        sense_diacnts.iter_mut().for_each(|v| *v = 0);
         let unwrapped = line?;
         let head = unwrapped.trim();
 
@@ -114,6 +152,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         };
 
         let n_senses = expected_pi(&vm.counts, vm.alpha, x, &mut z, args.sense_threshold);
+        let head_mid = x;
 
         for zk in z.iter_mut() {
             if *zk < args.sense_threshold { *zk = 0.; }
@@ -131,85 +170,91 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         };
 
         let poss = posattr.revidx().id2poss(corpid);
-
         for pos in poss {
-            eprintln!("{}", pos);
+            let structpos = if let Some(structpos) = diastruct.num_at_pos(pos) {
+                structpos
+            } else {
+                eprintln!("WARN: position {} for id {} is outside structure", pos, corpid);
+                continue
+            };
+
+            let diaid = diastructattr.text().get(structpos);
+
+            let epoch_no = diamap[diaid as usize];
+            if epoch_no == u32::MAX {
+                continue;
+            }
+
+            //eprintln!("{}", pos);
+            //if pos & 0xfff == 0 && Instant::now() >= next_report_time {
+            //    eprintln!("visited {} positions out of {} ({:.2} %)",
+            //        pos, h, 100.*(pos as f64)/(text_size as f64));
+            //    next_report_time += Duration::from_secs(240);
+            //}
+            let start = if pos >= ntokens as u64 { pos - ntokens as u64 } else { 0 };
+            let ctxit = posattr.iter_ids(start);
+            lctx.clear(); rctx.clear();
+
+            for (ctxpos, ctx_cid) in std::iter::zip(start.., ctxit) {
+                if ctxpos == pos { continue; }
+                if ctxpos > pos + ntokens as u64 {
+                    break;
+                }
+                if ctxpos < pos {
+                    lctx.push(ctx_cid);
+                } else {
+                    rctx.push(ctx_cid);
+                }
+            }
+
+            let fmap_ids = |corpid: &u32| {
+                match corpid2id[*corpid as usize] {
+                    u32::MAX => None,
+                    id => Some(id),
+                }
+            };
+
+            for ctx_mid in lctx.iter()
+                    .rev().filter_map(fmap_ids).take(args.window) {
+                var_update_z(&vm.in_vecs, &vm.out_vecs, &vm.code, &vm.path,
+                    head_mid, ctx_mid, &mut z);
+            }
+
+            for ctx_mid in rctx.iter()
+                    .filter_map(fmap_ids).take(args.window) {
+                var_update_z(&vm.in_vecs, &vm.out_vecs, &vm.code, &vm.path,
+                    head_mid, ctx_mid, &mut z);
+            }
+
+            exp_normalize(&mut z);
+            for (i, zk) in z.iter().enumerate() {
+                zst[i].push(*zk);
+            }
+
+            let maxsense = 0usize;
+            sense_diacnts[epoch_no as usize*nmeanings + maxsense] += 1;
         }
 
-    }
+        let freqs = (0..epochcnt)
+            .map(|epoch|
+                (0..nmeanings).map(|sense| sense_diacnts[epoch*nmeanings + sense]).sum()
+            ).collect::<Vec<u64>>();
 
-
-    let h = posattr.id_range() as usize;
-    let w = new_norms.len();
-    eprintln!("w {}", w);
-
-    if w < 2 {
-        eprintln!("WARNING: less than 2 valid structattr values");
-        return Err("semantic error".into());
-    }
-
-    if h <= 0 {
-        eprintln!("WARNING: empty corpus");
-        return Err("semantic error".into());
-    }
-    
-    eprintln!("using {} diachronic attribute ids and {} ids ({} MB)",
-              w, h, 8 * w * h / 1024 / 1024);
-    let mut freqs = vec![0f64; w * h];
-
-    let mut diaiditer = diastructattr.iter_ids(0);
-    let mut structpos = 0u64;
-    let mut diaid = diaiditer.next().unwrap();
-    let mut structbeg = diastruct.beg_at(structpos) as usize;
-    let mut structend = diastruct.end_at(structpos) as usize;
-
-    let mut next_report_time = Instant::now() + Duration::from_secs(60);
-    let text_size = posattr.text().size();
-    for (pos, attr_id) in std::iter::zip(
-            0..text_size,
-            posattr.text().posat(0).unwrap()) {
-        if pos & 0xfff == 0 && Instant::now() >= next_report_time {
-            eprintln!("visited {} positions out of {} ({:.2} %)",
-                pos, h, 100.*(pos as f64)/(text_size as f64));
-            next_report_time += Duration::from_secs(240);
+        println!("HW {}", head);
+        for sense in 0..nmeanings as usize {
+            print!("s##{}", sense);
+            for epoch in 0..epochcnt {
+                print!("\t{}", sense_diacnts[epoch*nmeanings + sense]);
+            }
+            println!();
         }
-        while pos >= structend {
-            structpos += 1;
-            diaid = diaiditer.next().unwrap();
-            structbeg = diastruct.beg_at(structpos) as usize;
-            structend = diastruct.end_at(structpos) as usize;
+        print!("f");
+        for epoch in 0..epochcnt {
+            print!("\t{}", freqs[epoch]);
         }
-        if pos < structbeg {
-            continue;
-        }
+        println!();
 
-        let epoch_no = diamap[diaid as usize];
-        if epoch_no == u32::MAX { continue; }
-        freqs[attr_id as usize * w + epoch_no as usize] += 1.;
-    }
-
-    eprintln!("frequencies collected, calculating trends");
-    let mut mktf = std::fs::File::create(trendbase.clone() + ".mkts_all.trends")?;
-    let mut mktwr = BinaryTrendsWriter::new(&mut mktf)?;
-    let mut mkmf = std::fs::File::create(trendbase.clone() + ".mkts_all.minigraphs")?;
-    let mut mkmwr = BinaryMinigraphWriter::new(&mut mkmf)?;
-
-    let mut lrtf = std::fs::File::create(trendbase.clone() + ".linreg_all.trends")?;
-    let mut lrtwr = BinaryTrendsWriter::new(&mut lrtf)?;
-    let mut lrmf = std::fs::File::create(trendbase.clone() + ".linreg_all.minigraphs")?;
-    let mut lrmwr = BinaryMinigraphWriter::new(&mut lrmf)?;
-
-    let mut normed = vec![0.0f64; w];
-    let mut rel = vec![0.0f64; w];
-    let mut next_report_time = Instant::now() + Duration::from_secs(60);
-    let xs = (0..w).map(|x| x as f64).collect::<Vec<_>>();
-    for attr_id in 0..posattr.id_range() as usize {
-        if attr_id & 0xfff == 0 && Instant::now() >= next_report_time {
-            eprintln!("visited {} ids out of {} ({:.2} %)",
-                attr_id, h, 100.*(attr_id as f64)/(h as f64));
-            next_report_time += Duration::from_secs(240);
-        }
-
+        /*
         let mut sum_normed = 0.0f64;
         for j in 0..w {
             normed[j] = if new_norms[j] != 0.0f64 {
@@ -227,32 +272,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         let (lp, lslope) = adagram::diachronic::linreg(&xs[..], &rel);
         let (mp, mslope) = adagram::diachronic::mk(&xs[..], &rel);
+        */
 
-        let lslope_over_all_periods = lslope * (w as f64 - 1.);
-        let mslope_over_all_periods = mslope * (w as f64 - 1.);
-
-        let langle = lslope_over_all_periods.atan() * (180./3.141592);
-        let mangle = mslope_over_all_periods.atan() * (180./3.141592);
-
-        mktwr.put(attr_id as u32, mangle.round() as i8, mp as f32)?;
-        lrtwr.put(attr_id as u32, langle.round() as i8, lp as f32)?;
-
-        let mut resampled = vec![0f64; 8];
-        resample(&rel[..], &mut resampled[..]);
-        let decimated = resampled.into_iter().map(|n| (n*6.0) as u8).collect::<Vec::<u8>>();
-        mkmwr.put(attr_id as u32, &decimated)?;
-        lrmwr.put(attr_id as u32, &decimated)?;
     }
-
-    use std::io::Write;
-    mktf.flush()?;
-    mkmf.flush()?;
-    lrtf.flush()?;
-    lrmf.flush()?;
-    mktf.sync_all()?;
-    mkmf.sync_all()?;
-    lrtf.sync_all()?;
-    lrmf.sync_all()?;
 
     eprintln!("done.");
     Ok(())
